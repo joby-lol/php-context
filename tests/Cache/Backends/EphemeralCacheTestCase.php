77daf4ce49b9720ea036fa7adaec7e6f75e46d11
Joby Elliott <code@byjoby.com>
2025-10-29 07:22:12 -0600

    updated license information and repository links

--- tests/Cache/Backends/EphemeralCacheTestCase.php
+++ tests/Cache/Backends/EphemeralCacheTestCase.php
@@ -1,4 +1,10 @@
 <?php
+/*
+* Context Injection
+* https://github.com/joby-lol/php-context
+* (c) 2024-2025 Joby Elliott code@joby.lol
+* MIT License https://opensource.org/licenses/MIT
+*/
 
 namespace Joby\ContextInjection\Cache\Backends;
 
--- tests/Invoker/DefaultInvokerTest.php
+++ tests/Invoker/DefaultInvokerTest.php
@@ -1,4 +1,10 @@
-<?php /** @noinspection PhpUnhandledExceptionInspection */
+<?php
+/*
+* Context Injection
+* https://github.com/joby-lol/php-context
+* (c) 2024-2025 Joby Elliott code@joby.lol
+* MIT License https://opensource.org/licenses/MIT
+*/
 
 namespace Joby\ContextInjection\Invoker;
 
